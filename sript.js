20d880b74a5a80ae8c615370f76d631a06a11f1c
Sven Huff <info@sven-huff.de>
2020-05-25 18:35:33 +0200

    HTML & Style

---
+++ sript.js
@@ -0,0 +1,40 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['Streich', 'Grube', 'Polka']
+
+let selectedWord = words[Math.floor(Math.random() * words.length)]
+console.log(selectedWord);
+
+const correctLetters = [];
+const wrongLetters = [];
+
+function displayWord() {
+    wordEl.innerHTML = `
+    ${selectedWord
+    .split('')
+    .map(
+        letter => ` 
+        <span class="letter">
+        ${correctLetters.includes(letter) ? letter : ''} 
+        </span>
+    `
+    )
+    .join('')}
+    `;
+
+    const innerWord = wordEl.innerText.replace(/\n/g, '');
+
+    if (innerWord === selectedWord) {
+        finalMessage.innerText = 'Congratulations you won';
+        popup.style.display = 'flex';
+    }
+}
+
+displayWord();
