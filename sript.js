8fb898b0565a78a9b1a4af6f9bdc09acde1f1017
Sven Huff <info@sven-huff.de>
2020-05-29 10:26:43 +0200

    finishing js

--- sript.js
+++ sript.js
@@ -37,4 +37,61 @@ function displayWord() {
     }
 }
 
+// Update the wrong letters
+
+function updateWrongLettersEl() {
+    wrongLettersEl.innerHTML = `
+   ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+   ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+   `;
+
+    figureParts.forEach((part, index) => {
+        const errors = wrongLetters.length;
+
+        if (index < errors) {
+            part.style.display = 'block';
+        } else {
+            part.style.display = 'none';
+        }
+    })
+
+}
+
+// show notification
+
+function showNotification() {
+    notification.classList.add('show');
+
+    setTimeout(() => {
+        notification.classList.remove('show')
+    }, 2000);
+}
+
+//keydown letter press
+window.addEventListener('keydown', e => {
+    // console.log(e.keyCode);
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+        const letter = e.key;
+
+        if (selectedWord.includes(letter)) {
+            if (!correctLetters.includes(letter)) {
+                correctLetters.push(letter);
+
+                displayWord();
+            } else {
+                showNotification();
+            }
+        } else {
+            if (!wrongLetters.includes(letter)) {
+                wrongLetters.push(letter);
+
+                updateWrongLettersEl();
+            } else {
+                showNotification();
+            }
+        }
+
+    }
+});
+
 displayWord();
